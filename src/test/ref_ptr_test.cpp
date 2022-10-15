21e70e44479549812aeb555c2405be66b94d2aac
The Fern <krede2000@gmail.com>
2022-10-15 22:26:24 +0200

    Fix compile error in ref_ptr_test

--- src/test/ref_ptr_test.cpp
+++ src/test/ref_ptr_test.cpp
@@ -23,7 +23,7 @@ namespace ktl::test
     KTL_ADD_TEST(test_ref_ptr_stack_double)
     {
         stack<128> block;
-        type_stack_allocator<double, 128> alloc({ block });
+        type_stack_allocator<double, 128> alloc(block);
 
         ref_ptr<double, type_stack_allocator<double, 128>> ptr = allocate_ref<double>(alloc, 42.0);
 
@@ -33,7 +33,7 @@ namespace ktl::test
     KTL_ADD_TEST(test_ref_ptr_complex)
     {
         stack<128> block;
-        type_stack_allocator<complex_t, 128> alloc({ block });
+        type_stack_allocator<complex_t, 128> alloc(block);
 
         ref_ptr<complex_t, type_stack_allocator<complex_t, 128>> ptr = allocate_ref<complex_t>(alloc, 42.0);
 
