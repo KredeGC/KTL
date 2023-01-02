b9a2da6942552479fc8ef9b7b846875ba6c41349
The Fern <krede2000@gmail.com>
2023-01-02 21:16:09 +0100

    segregator_builder now tries to balance the allocator types This should make the allocator more stable as the number of subsegragators increase

--- include/ktl/allocators/segragator_fwd.h
+++ include/ktl/allocators/segragator_fwd.h
@@ -16,74 +16,78 @@ namespace ktl
 
 	template<typename T, size_t Threshold, typename P, typename F>
 	using type_segragator_allocator = type_allocator<T, segragator<Threshold, P, F>>;
-    
-    // variadic builder
-    template<typename ...Args>
-    struct segragator_builder;
-    
-    template<typename Primary, typename Threshold, typename Fallback>
-    struct segragator_builder<Primary, Threshold, Fallback>
+
+    // Helper function for getting the closest power of 2
+    constexpr size_t pow2_ceil(size_t v)
     {
-        using type = segragator<Threshold::value, Primary, Fallback>;
-    };
-    
-    template<typename Alloc, typename Threshold, typename ...Args>
-    struct segragator_builder<Alloc, Threshold, Args...>
+        --v;
+        v |= v >> 1;
+        v |= v >> 2;
+        v |= v >> 4;
+        v |= v >> 8;
+        v |= v >> 16;
+        return ++v;
+    }
+
+    // Helper struct for splitting a tuple in 2
+    template<typename, typename, typename>
+    struct tuple_split;
+
+    template<std::size_t... N, std::size_t... M, typename T>
+    struct tuple_split<std::index_sequence<N...>, std::index_sequence<M...>, T>
     {
-        using type = segragator<Threshold::value, Alloc, typename segragator_builder<Args...>::type>;
-    };
-    
-    template<typename ...Args>
-    using segragator_builder_t = typename segragator_builder<Args...>::type;
-    
-    template<size_t N>
-    using threshold = std::integral_constant<size_t, N>;
-
-
-
-    template <class, class, class>
-    struct half_half;
-
-    template <std::size_t... N, std::size_t... M, class T>
-    struct half_half<std::index_sequence<N...>, std::index_sequence<M...>, T>
-    {
+        // The middle point is at sizeof...(N)
         using first = std::tuple<typename std::tuple_element_t<N, T>...>;
 
+        // Offset by N + 1, since we don't want the middle point in either
         using second = std::tuple<typename std::tuple_element_t<M + 1 + sizeof...(N), T>...>;
     };
 
+    // Recursive helper struct for generating the segragator type
+    template<typename... Ts>
+    struct segragator_builder;
 
-    template<class... Ts>
-    struct tuple_half;
-
-    // When there are only 3 types left
+    // When three types remain, use a segragator
     template<typename Primary, typename Threshold, typename Secondary>
-    struct tuple_half<std::tuple<Primary, Threshold, Secondary>>
+    struct segragator_builder<std::tuple<Primary, Threshold, Secondary>>
     {
         using type = segragator<Threshold::value, Primary, Secondary>;
     };
 
-    // Takes in a tuple of types
-    template<class... Ts>
-    struct tuple_half<std::tuple<Ts...>>
+    // When a single type remains, just return it
+    template<typename Alloc>
+    struct segragator_builder<std::tuple<Alloc>>
     {
-        using threshold = typename std::tuple_element_t<sizeof...(Ts) / 2, std::tuple<Ts...>>;
+        using type = Alloc;
+    };
 
-        using split = half_half<
-            std::make_index_sequence<sizeof...(Ts) / 2>,
-            std::make_index_sequence<sizeof...(Ts) / 2>,
+    // Takes in a tuple of types and returns the final segragator
+    template<class... Ts>
+    struct segragator_builder<std::tuple<Ts...>>
+    {
+        static_assert(sizeof...(Ts) % 2 != 0, "The number of allocators needs to match the number of thresholds + 1");
+
+        // The middle of the type is the root of a complete binary tree
+        // The root of a complete binary tree is (2^(log2(N)) - 1) / 2
+        static constexpr size_t max = sizeof...(Ts);
+        static constexpr size_t middle = (pow2_ceil(max) - 1) / 2;
+
+        using split = tuple_split<
+            std::make_index_sequence<middle>, // Up to, but not including the middle threshold
+            std::make_index_sequence<max - middle - 1>, // Anything after the middle threshold
             std::tuple<Ts...>>;
 
-        using first = typename tuple_half<typename split::first>::type;
-
-        using second = typename tuple_half<typename split::second>::type;
+        using threshold = typename std::tuple_element_t<middle, std::tuple<Ts...>>;
+        using first = typename segragator_builder<typename split::first>::type;
+        using second = typename segragator_builder<typename split::second>::type;
 
         using type = segragator<threshold::value, first, second>;
     };
 
-    template<class... Ts>
-    struct tuple_full
-    {
-        using type = typename tuple_half<std::tuple<Ts...>>::type;
-    };
+    // Helper type without the tuple
+    template<typename ...Ts>
+    using segragator_builder_t = typename segragator_builder<std::tuple<Ts...>>::type;
+
+    template<size_t N>
+    using threshold = std::integral_constant<size_t, N>;
 }
