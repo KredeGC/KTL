29a6afd7a1426775c8e065e56021225d9693b3f6
The Fern <krede2000@gmail.com>
2023-01-04 14:34:27 +0100

    Update segragator_builder to allow for left-lean Removed some redundant code in segragator_fwd

--- include/ktl/allocators/segragator_fwd.h
+++ include/ktl/allocators/segragator_fwd.h
@@ -36,66 +36,74 @@ namespace ktl
             };
         };
 
+
         // Helper struct for splitting a tuple based on indices
-        template<size_t, typename, typename, typename>
+        template<typename, typename, typename>
         struct tuple_split_indices;
 
-        template<size_t O, size_t... N, size_t... M, typename... T>
-        struct tuple_split_indices<O, std::index_sequence<N...>, std::index_sequence<M...>, std::tuple<T...>>
+        template<size_t... N, size_t... M, typename... T>
+        struct tuple_split_indices<std::index_sequence<N...>, std::index_sequence<M...>, std::tuple<T...>>
         {
             // The middle point is at sizeof...(N)
             using first = std::tuple<typename std::tuple_element_t<N, std::tuple<T...>>...>;
 
-            // Offset by N + O
-            using second = std::tuple<typename std::tuple_element_t<M + O + sizeof...(N), std::tuple<T...>>...>;
+            // Offset by N + 1, since we don't include the middle type
+            using second = std::tuple<typename std::tuple_element_t<M + 1 + sizeof...(N), std::tuple<T...>>...>;
         };
 
-        // Helper struct for splitting a tuple based on a middle point
-        template<size_t N, typename... Ts>
-        struct tuple_split_reverse
-            : tuple_split_indices<1,
-                std::make_index_sequence<sizeof...(Ts) - 1 - N>, // Up to, but not including the middle threshold
-                std::make_index_sequence<N>, // Anything after the middle threshold
-                std::tuple<Ts...>>
+
+        // Helper struct for inverting sizes
+        template<bool, size_t, size_t>
+        struct size_invert;
+
+        template<size_t N, size_t M>
+        struct size_invert<false, N, M>
         {
-            // This points to the root of a right-leaning complete binary tree
-            using threshold = typename std::tuple_element_t<sizeof...(Ts) - 1 - N, std::tuple<Ts...>>;
+            static constexpr size_t first = N;
+            static constexpr size_t second = M - 1 - N;
         };
+
+        template<size_t N, size_t M>
+        struct size_invert<true, N, M>
+        {
+            static constexpr size_t first = M - 1 - N;
+            static constexpr size_t second = N;
+        };
+
 
         // Recursive helper struct for generating the segragator type
-        template<typename...>
+        template<bool, typename...>
         struct segragator_builder;
 
-        // When three types remain, use a segragator
-        template<typename Primary, typename Threshold, typename Secondary>
-        struct segragator_builder<std::tuple<Primary, Threshold, Secondary>>
-        {
-            using type = segragator<Threshold::value, Primary, Secondary>;
-        };
-
         // When a single type remains, just return it
-        template<typename Alloc>
-        struct segragator_builder<std::tuple<Alloc>>
+        template<bool R, typename Alloc>
+        struct segragator_builder<R, std::tuple<Alloc>>
         {
             using type = Alloc;
         };
 
         // Takes in a tuple of types and returns the final segragator
-        template<typename... Ts>
-        struct segragator_builder<std::tuple<Ts...>>
+        template<bool R, typename... Ts>
+        struct segragator_builder<R, std::tuple<Ts...>>
         {
             static_assert(sizeof...(Ts) % 2 != 0, "The number of allocators needs to match the number of thresholds + 1");
 
             // The middle of the parameter pack is the root of a complete binary tree
             // The root of a complete binary tree is (2^(log2(N)) - 1) / 2
-            using split = tuple_split_reverse<
-                (pow2<sizeof...(Ts)>::Result - 1) / 2,
-                Ts...>;
+            using middle = typename size_invert<R, (pow2<sizeof...(Ts)>::Result - 1) / 2, sizeof...(Ts)>;
+
+            // Split the parameter pack into 2 tuples
+            using split = tuple_split_indices<
+                std::make_index_sequence<middle::first>, // Up to, but not including the middle threshold
+                std::make_index_sequence<middle::second>, // Anything after the middle threshold
+                std::tuple<Ts...>>;
+
+            // This points to the root of a complete binary tree
+            using threshold = typename std::tuple_element_t<middle::first, std::tuple<Ts...>>;
 
             // Create this struct recursively for both sides of the middle point
-            using threshold = typename split::threshold;
-            using first = typename segragator_builder<typename split::first>::type;
-            using second = typename segragator_builder<typename split::second>::type;
+            using first = typename segragator_builder<R, typename split::first>::type;
+            using second = typename segragator_builder<R, typename split::second>::type;
 
             using type = segragator<threshold::value, first, second>;
         };
@@ -103,7 +111,10 @@ namespace ktl
 
     // Helper type without the tuple
     template<typename ...Ts>
-    using segragator_builder_t = typename detail::segragator_builder<std::tuple<Ts...>>::type;
+    using segragator_builder_l = typename detail::segragator_builder<false, std::tuple<Ts...>>::type;
+
+    template<typename ...Ts>
+    using segragator_builder_r = typename detail::segragator_builder<true, std::tuple<Ts...>>::type;
 
     template<size_t N>
     using threshold = std::integral_constant<size_t, N>;
