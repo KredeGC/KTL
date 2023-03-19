372bdd1d6251d188e3751c03f80155ea5ad37d4c
The Fern <krede2000@gmail.com>
2023-03-19 15:52:41 +0100

    Refactor overflow allocator to take a templated stream as argument Update README

--- include/ktl/allocators/overflow_fwd.h
+++ include/ktl/allocators/overflow_fwd.h
@@ -10,24 +10,24 @@
 namespace ktl
 {
     // overflow
-	template<typename Alloc, std::ostream& Stream>
+	template<typename Alloc, typename Stream = std::ostream>
 	class overflow;
 
 	/**
 	 * @brief Shorthand for a typed overflow allocator
 	*/
-	template<typename T, typename Alloc, std::ostream& Stream>
+	template<typename T, typename Alloc, typename Stream = std::ostream>
 	using type_overflow_allocator = type_allocator<T, overflow<Alloc, Stream>>;
 
 	/**
 	 * @brief Shorthand for a typed, ref-counted overflow allocator
 	*/
-	template<typename T, typename Alloc, std::ostream& Stream>
+	template<typename T, typename Alloc, typename Stream = std::ostream>
 	using type_shared_overflow_allocator = type_allocator<T, shared<overflow<Alloc, Stream>>>;
 
 	/**
 	 * @brief Shorthand for a typed, thread-safe, ref-counted overflow allocator
 	*/
-	template<typename T, typename Alloc, std::ostream& Stream>
+	template<typename T, typename Alloc, typename Stream = std::ostream>
 	using type_threaded_overflow_allocator = type_allocator<T, threaded<overflow<Alloc, Stream>>>;
 }
